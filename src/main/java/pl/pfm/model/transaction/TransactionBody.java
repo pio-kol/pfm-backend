baac50b20449951135175cf659fa0bfd25257d89
AniaLew <ania_lew@o2.pl>
2017-10-13 12:37:27 +0200

    Dependencies added.

--- src/main/java/pl/pfm/model/transaction/TransactionBody.java
+++ src/main/java/pl/pfm/model/transaction/TransactionBody.java
@@ -1,5 +1,7 @@
 package pl.pfm.model.transaction;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import pl.pfm.model.account.Account;
 import pl.pfm.model.category.Category;
 
@@ -7,7 +9,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class TransactionBody {
-  private LocalDate date;
+  private LocalDate date = LocalDate.now();
   private String description;
   private String comment;
   private Category category;
@@ -16,9 +18,11 @@ public class TransactionBody {
 
   public TransactionBody(){
   }
-
-  public TransactionBody(LocalDate date, String description, String comment,
-      Category category, Account account, BigDecimal price) {
+  @JsonCreator
+  public TransactionBody(@JsonProperty("date") LocalDate date,
+      @JsonProperty("description") String description, @JsonProperty("comment") String comment,
+      @JsonProperty("category") Category category, @JsonProperty("account") Account account,
+      @JsonProperty("price") BigDecimal price) {
     this.date = date;
     this.description = description;
     this.comment = comment;
